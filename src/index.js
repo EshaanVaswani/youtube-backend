9cb07a9890534b4a00d2dd98849df5131e8d3e68
Eshaan Vaswani <eshaanv2004@gmail.com>
2024-04-20 01:53:05 +0530

    custom api responses and error handling

--- src/index.js
+++ src/index.js
@@ -1,4 +1,18 @@
 import "dotenv/config";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-connectDB();
+connectDB()
+   .then(() => {
+      const port = process.env.PORT || 8000;
+      app.on("error", (err) => {
+         console.log("ERROR :: EXPRESS APP ON :: ", err);
+         throw err;
+      });
+      app.listen(port, () => {
+         console.log(`Server is running on port : ${port}`);
+      });
+   })
+   .catch((err) => {
+      console.log("ERROR :: MONGODB CONNECTION FAILED :: ", err);
+   });

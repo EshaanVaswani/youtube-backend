45ba0e77cae8d91d5767bb45a02ea8aaf55f072f
Eshaan Vaswani <eshaanv2004@gmail.com>
2024-05-03 14:17:25 +0530

    add comment, like, playlist, tweet models

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -160,8 +160,8 @@ const logoutUser = asyncHandler(async (req, res) => {
    await User.findByIdAndUpdate(
       req.user._id,
       {
-         $set: {
-            refreshToken: undefined,
+         $unset: {
+            refreshToken: 1, // this removes the field from document
          },
       },
       {
@@ -215,17 +215,17 @@ const refreshAccessToken = asyncHandler(async (req, res) => {
          secure: true,
       };
 
-      const { accessToken, newRefreshToken } =
+      const { accessToken, refreshToken } =
          await generateAccessAndRefreshTokens(user._id);
 
       return res
          .status(200)
          .cookie("accessToken", accessToken, options)
-         .cookie("refreshToken", newRefreshToken, options)
+         .cookie("refreshToken", refreshToken, options)
          .json(
             new ApiResponse(
                200,
-               { accessToken, refreshToken: newRefreshToken },
+               { accessToken, refreshToken },
                "Access and refresh token generated successfully"
             )
          );

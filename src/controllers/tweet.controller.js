07b364019b3ff0c09e59df5e0ffac4c556cb6550
Eshaan Vaswani <eshaanv2004@gmail.com>
2024-05-11 02:07:54 +0530

    add all routes and test tweets functionality

--- src/controllers/tweet.controller.js
+++ src/controllers/tweet.controller.js
@@ -4,6 +4,7 @@ import { User } from "../models/user.model.js";
 import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 import { asyncHandler } from "../utils/asyncHandler.js";
+import { Like } from "../models/like.model.js";
 
 const createTweet = asyncHandler(async (req, res) => {
    const { content } = req.body;
@@ -66,13 +67,13 @@ const getUserTweets = asyncHandler(async (req, res) => {
       },
       {
          $addFields: {
-            $likeCount: {
+            likeCount: {
                $size: "$likes",
             },
-            $owner: {
+            owner: {
                $first: "$owner",
             },
-            $isLiked: {
+            isLiked: {
                $cond: {
                   if: { $in: [req.user?._id, "$likes.likedBy"] },
                   then: true,
@@ -106,7 +107,7 @@ const updateTweet = asyncHandler(async (req, res) => {
 
    const tweet = await Tweet.findById(tweetId);
 
-   if (tweet.owner !== req.user?._id) {
+   if (tweet.owner.toString() !== req.user?._id.toString()) {
       throw new ApiError(401, "Unauthorized request to update tweet");
    }
 
@@ -138,13 +139,19 @@ const deleteTweet = asyncHandler(async (req, res) => {
       throw new ApiError(404, "Tweet not found");
    }
 
-   if (tweet.owner !== req.user?._id) {
+   if (tweet.owner.toString() !== req.user?._id.toString()) {
       throw new ApiError(401, "Unauthorized request to delete tweet");
    }
 
-   await Tweet.findByIdAndDelete(tweetId);
+   const deletedTweet = await Tweet.findByIdAndDelete(tweetId);
 
-   res.status(200).json(new ApiResponse(200, {}, "Tweet deleted successfully"));
+   if (deletedTweet) {
+      await Like.deleteMany({ tweet: tweetId });
+   }
+
+   return res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Tweet deleted successfully"));
 });
 
 export { createTweet, getUserTweets, updateTweet, deleteTweet };
